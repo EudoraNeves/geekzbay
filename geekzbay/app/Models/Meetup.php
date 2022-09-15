19bece5cf573c64b9f908a7ac13f107d8d5c4b2c
Shdorsh <am.bre@outlook.de>
2022-09-15 11:08:53 +0200

    creating the models

---
+++ geekzbay/app/Models/Meetup.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meetup extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'date',
+        'desc',
+        'eventadmin_id',
+        'coomunity_id',
+        'location_id',
+        'alt_address_city',
+        'alt_address_street',
+        'alt_address_number',
+    ];
+}
