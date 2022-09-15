83664f13afc540699569310fe89c8a5fb9b2b604
Shdorsh <am.bre@outlook.de>
2022-09-15 15:34:02 +0200

    Location skeleton
    Adding some html tags to it

--- geekzbay/resources/views/layouts/locations.blade.php
+++ geekzbay/resources/views/layouts/locations.blade.php
@@ -2,4 +2,25 @@
 @section('title', 'locations')
 @section('main')
     locations Page
+
+    <!-- Location big card: row->column -->
+    <div class="d-flex flex-row">
+        <!-- Image and stars/likes: column -->
+        <div class="d-flex d-column">
+            <img src="" alt="" />
+            <!-- Stars and likes: rows -->
+            <div class="d-flex flex-row">
+
+            </div>
+        </div>
+
+        <!-- Address stuff: column -->
+        <div class="d-flex flex-column">
+            <div>Town:</div>
+            <div>Street:</div>
+            <div>Number:</div>
+            <div>Communities</div>
+        </div>
+    </div>
+
 @endsection
