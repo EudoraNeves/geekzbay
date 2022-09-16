f3a5d11384d6c43aa700622b77a53885ef0ec544
EudoraNeves <eudoraneves@outlook.com>
2022-09-16 16:20:50 +0200

    change mail_host from mailhog to 127.0.0.1

--- geekzbay/app/Http/Controllers/Auth/RegisteredUserController.php
+++ geekzbay/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,8 +49,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        // return redirect(RouteServiceProvider::HOME);
-        return redirect()->route('home');
-
+        return redirect(RouteServiceProvider::HOME);
     }
 }
