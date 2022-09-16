8555381612610e9ea10b5175f26e0bf6116f38a1
Skinyan <106683903+Skinyan@users.noreply.github.com>
2022-09-16 10:44:47 +0200

    added community page

--- geekzbay/resources/views/layouts/community.blade.php
+++ geekzbay/resources/views/layouts/community.blade.php
@@ -2,4 +2,29 @@
 @section('title', 'community')
 @section('main')
     Community Page
+    <div>
+        <h1> <i>Welcome to our Community Page</i></h1>
+        <h2>Here you can find the community you are interested</h2>
+    </div>
+    <div>
+        <h2>Anime and Manga Community</h2>
+        <div></div>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Gaming Community</h2>
+        <div></div>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Cardgame Community</h2>
+        <div></div>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Boardgames Community</h2>
+        <div></div>
+        <img src="" alt="">
+    </div>
+
 @endsection
