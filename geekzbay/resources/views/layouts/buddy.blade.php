901125f6687917cdb64f64d3cc70c456f60e973b
EudoraNeves <eudoraneves@outlook.com>
2022-09-18 02:01:26 +0200

    first show question mark, add after-clicking effect

--- geekzbay/resources/views/layouts/buddy.blade.php
+++ geekzbay/resources/views/layouts/buddy.blade.php
@@ -2,6 +2,31 @@
 @section('title', 'buddy')
 @section('css')
     <link rel="stylesheet" href="css/buddy.css">
+    <style>
+        .hidden {
+            display: none;
+        }
+
+        .effect {
+            animation: blink 1s infinite;
+        }
+
+        .question-mark {
+            font-size: 300px;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        @keyframes blink {
+            0% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 100%;
+            }
+        }
+    </style>
 @endsection
 @section('main')
     <div class="container">
@@ -20,11 +45,15 @@
                         imgSrc="{{ Auth::user()->profilePicture }}" imgAlt="{{ Auth::user()->name }}'s profile photo" />
                 </div>
                 <div class="findBuddy_btn">
-                    <button type="button" class="btn btn-warning"><a href="{{ route('buddy') }}">Find Buddy</a></button>
+                    <button type="button" class="btn btn-warning">Find Buddy</button>
                 </div>
-                <div class="buddyProfile">
-                    <x-buddy-card self="false" username="{{ $randomBuddy->name }}" quote="{{ $randomBuddy->name }}'s quote"
-                        imgSrc="{{ $randomBuddy->profilePicture }}" imgAlt="{{ $randomBuddy->name }}'s profile photo" />
+                <div class="buddyProfile hidden">
+                    <x-buddy-card self="false" username="{{ $randomBuddy->name }}"
+                        quote="{{ $randomBuddy->name }}'s quote" imgSrc="{{ $randomBuddy->profilePicture }}"
+                        imgAlt="{{ $randomBuddy->name }}'s profile photo" />
+                </div>
+                <div class="card question-mark" style="width: 18rem;">
+                    ?
                 </div>
             </section>
         @else
@@ -33,11 +62,32 @@
             </div>
         @endif
     </div>
-    {{-- <script>
+    <script>
+        let buddyProfile = document.querySelector('.buddyProfile')
+        let questionmark = document.querySelector('.question-mark')
+
+        let hidden = true
+        if (document.referrer === document.URL) {
+            hidden = false
+            buddyProfile.classList.remove('hidden')
+            questionmark.classList.add('hidden')
+        }
+
         function fetchRandomBuddy() {
-            window.location.reload();
+            if (!hidden) {
+                location.reload()
+                //window.location.reload();
+            } else {
+                // hidden = false
+                questionmark.classList.add('effect')
+                setTimeout(() => {
+                    hidden = false
+                    buddyProfile.classList.remove('hidden')
+                    questionmark.classList.add('hidden')
+                }, 2000);
+            }
         }
         let findBuddy_btn = document.querySelector('.findBuddy_btn > button');
         findBuddy_btn.addEventListener('click', fetchRandomBuddy);
-    </script> --}}
+    </script>
 @endsection
