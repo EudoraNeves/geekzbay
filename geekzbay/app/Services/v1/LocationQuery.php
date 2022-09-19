3d9a2ded412c0e9a3d9567558329839fdf785204
Shdorsh <am.bre@outlook.de>
2022-09-19 19:59:33 +0200

    adding more functionality to the api

--- geekzbay/app/Services/v1/LocationQuery.php
+++ geekzbay/app/Services/v1/LocationQuery.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Services\v1;
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-class LocationQuery {
+class LocationQuery extends Query {
+    // The parameters one can search through, as well as the query applied by it.
     protected $allowedParams = [
+        'name' => 'nameFilter',
+        'category' => 'categoryFilter',
     ];
+
+    protected function nameFilter($query) {
+        return [Str::of('name')->lower(), 'LIKE', `%$query%`];
+    }
+
+    protected function categoryFilter($query) {
+        return['category_id','=', $query];
+    }
 }
 
 ?>
--- geekzbay/app/Services/v1/MeetupQuery.php
+++ geekzbay/app/Services/v1/MeetupQuery.php
@@ -1,10 +1,37 @@
 <?php
 
 namespace App\Services\v1;
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-class MeetupQuery {
+class MeetupQuery extends Query {
+    // The parameters one can search through, as well as the query applied by it.
+    protected $allowedParams = [
+        'name' => 'nameFilter',
+        'category' => 'categoryFilter',
+        'community' => 'communityFilter',
+        'startDate' => 'startDateFilter',
+        'endDate' => 'endDateFilter',
+    ];
 
+    protected function nameFilter($query) {
+        return [Str::of('name')->lower(), 'LIKE', `%$query%`];
+    }
+
+    protected function categoryFilter($query) {
+        return ['category_id','=', $query];
+    }
+
+    protected function communityFilter($query) {
+        return ['community_id', '=', $query];
+    }
+
+    protected function startDateFilter($query) {
+        return ['date','>=', $query];
+    }
+
+    protected function endDateFilter($query) {
+        return ['date','<=', $query];
+    }
 }
 
 ?>
