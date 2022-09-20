f27394a37a132b477123dc81e5e2e92217d6fee7
Shdorsh <am.bre@outlook.de>
2022-09-20 11:03:41 +0200

    icon fix
    works now, but anime focal is missing

--- geekzbay/database/seeders/CommunitySeeder.php
+++ geekzbay/database/seeders/CommunitySeeder.php
@@ -18,7 +18,7 @@ class CommunitySeeder extends Seeder
         //
         DB::table('communities')->insert(
             [
-                'img' => 'public/magic.png',
+                'img' => 'magic.png',
                 'name' => 'Magic: The Gathering',
                 'discordLink' => 'https://discord.gg/TkGhxGmsgff',
                 'category_id' => 3
@@ -26,7 +26,7 @@ class CommunitySeeder extends Seeder
         );
         DB::table('communities')->insert(
             [
-                'img' => 'public/anime_focal.png',
+                'img' => 'anime_focal.png',
                 'name' => 'Anime Focal',
                 'discordLink' => 'https://discord.gg/TkGhxGmB',
                 'category_id' => 1
@@ -34,7 +34,7 @@ class CommunitySeeder extends Seeder
         );
         DB::table('communities')->insert(
             [
-                'img' => 'public/Komga.png',
+                'img' => 'Komga.png',
                 'name' => 'Komga',
                 'discordLink' => 'https://discord.gg/komga-678794935368941569',
                 'category_id' => 1,
@@ -43,7 +43,7 @@ class CommunitySeeder extends Seeder
 
         DB::table('communities')->insert(
             [
-                'img' => 'public/Sweetspot.png',
+                'img' => 'Sweetspot.png',
                 'name' => 'SweetSpot Asbl',
                 'discordLink' => 'https://discord.gg/6Kphx7876',
                 'category_id' => 6,
@@ -53,7 +53,7 @@ class CommunitySeeder extends Seeder
 
         DB::table('communities')->insert(
             [
-                'img' => 'public/Let_Smash.png',
+                'img' => 'Let_Smash.png',
                 'name' => 'Letz Smash',
                 'discordLink' => 'https://discord.gg/dMU4TVm3',
                 'category_id' => 6,
@@ -61,7 +61,7 @@ class CommunitySeeder extends Seeder
         );
         DB::table('communities')->insert(
             [
-                'img' => 'public/gaming.png',
+                'img' => 'gaming.png',
                 'name' => 'Fighting Corner',
                 'discordLink' => 'discord.gg/2YeuPBR22X',
                 'category_id' => 6,
@@ -69,7 +69,7 @@ class CommunitySeeder extends Seeder
         );
         DB::table('communities')->insert(
             [
-                'img' => 'public/Wargame.png',
+                'img' => 'Wargame.png',
                 'name' => 'lux wargame',
                 'discordLink' => 'discord.gg/2YeuPfdgsdBR22X',
                 'category_id' => 2,
