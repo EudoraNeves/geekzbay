b31d74d801a1fd70774668d7541fcaca02032e24
Shdorsh <am.bre@outlook.de>
2022-09-20 10:02:24 +0200

    Fixing by adding a new location route

--- geekzbay/routes/web.php
+++ geekzbay/routes/web.php
@@ -31,6 +31,7 @@ Route::get('/community', [CommunityController::class, 'show'])->name('community'
 Route::get('/community/my-communities',[CommunityController::class, 'index'])->name('my-communities');
 // Locations
 Route::get('locations', [LocationController::class, 'show'])->name('locations');
+Route::get('location/{id}', [LocationController::class, 'show'])->name('location');
 Route::get('/locations/my-locations', [LocationController::class, 'index'])->name('my-locations');
 
 
