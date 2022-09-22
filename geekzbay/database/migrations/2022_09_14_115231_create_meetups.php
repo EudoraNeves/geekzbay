e2f81853097e21e46fa7a105a7dd6879ba6a9510
Shdorsh <am.bre@outlook.de>
2022-09-22 15:45:10 +0200

    pretend this was a different branch
    fixing meetup creation

--- geekzbay/database/migrations/2022_09_14_115231_create_meetups.php
+++ geekzbay/database/migrations/2022_09_14_115231_create_meetups.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->foreignId('eventadmin_id')->references('id')->on('users');
             $table->foreignId('community_id')->references('id')->on('communities');
             $table->foreignId('location_id')->references('id')->on('locations');
-            $table->text('alt_address_city')->nullabl();
+            $table->text('alt_address_city')->nullable();
             $table->text('alt_address_street')->nullable();
             $table->text('alt_address_number')->nullable();
             $table->timestamps();
