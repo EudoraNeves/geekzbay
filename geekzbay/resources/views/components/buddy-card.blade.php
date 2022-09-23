dacec4d58bb1184e84a3c784fc3dbdb5fc63f2fa
EudoraNeves <eudoraneves@outlook.com>
2022-09-23 21:07:33 +0200

    fix buddy issues

--- geekzbay/resources/views/components/buddy-card.blade.php
+++ geekzbay/resources/views/components/buddy-card.blade.php
@@ -19,11 +19,12 @@
     @endif
     <div class="card-body">
         @if ($self)
-            <a href="{{ route('profile') }}" class="card-link">Go to Profile</a>
+            <a href="{{ route('my-profile') }}" class="card-link">Go to Profile</a>
         @endif
         @if (!$self)
-            <button type="button" class="btn btn-warning addbuddy"><a
-                    href="<a>{{ route('addBuddy', ['buddy_id' => $addBuddyId]) }}">Add Buddy</a></button>
+            <button type="button" class="btn btn-warning addbuddy">
+                <a href="{{ route('addBuddy', ['buddy_id' => $addBuddyId]) }}">Add Buddy</a>
+            </button>
         @endif
     </div>
 </div>
