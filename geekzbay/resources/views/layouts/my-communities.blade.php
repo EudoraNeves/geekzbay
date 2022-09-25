8fe5a2f84ff17f01b9f2349a7bd5ae3362328ec9
EudoraNeves <eudoraneves@outlook.com>
2022-09-25 15:20:39 +0200

    show messages when user has no liked communities, prevent adding to DB if already existing

--- geekzbay/resources/views/layouts/my-communities.blade.php
+++ geekzbay/resources/views/layouts/my-communities.blade.php
@@ -5,21 +5,28 @@
 @endsection
 @section('main')
     <div class="my-communities d-flex gap-3 flex-wrap justify-content-center align-items-center p-5">
-        @foreach ($myCommunities as $community)
-            <div class="community w-25 mw-20 p-3 d-flex flex-column align-items-center justify-content-center gap-2">
-                <h3 class="name">{{ $community->name }}</h3>
-                <div class="w-100 d-flex justify-content-center">
-                    <img class="communityImg w-50" src="../assets/images/{{ $community->img }}" alt="{{ $community->name }}">
+        @if ($myCommunities->count() > 0)
+            @foreach ($myCommunities as $community)
+                <div class="community w-25 mw-20 p-3 d-flex flex-column align-items-center justify-content-center gap-2">
+                    <h3 class="name">{{ $community->name }}</h3>
+                    <div class="w-100 d-flex justify-content-center">
+                        <img class="communityImg w-50" src="../assets/images/{{ $community->img }}"
+                            alt="{{ $community->name }}">
+                    </div>
+                    <div>
+                        <p>{{ $community->desc }}</p>
+                    </div>
+                    <div class="discord">
+                        <a href="{{ $community->discordLink }}">
+                            <img class="discordIcon" src="../discord-icon.svg" alt="discord">
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <p>{{ $community->desc }}</p>
-                </div>
-                <div class="discord">
-                    <a href="{{ $community->discordLink }}">
-                        <img class="discordIcon" src="../discord-icon.svg" alt="discord">
-                    </a>
-                </div>
+            @endforeach
+        @else
+            <div class="msg d-flex align-items-center">
+                <p>You don't have any liked communities :(</p>
             </div>
-        @endforeach
+        @endif
     </div>
 @endsection
